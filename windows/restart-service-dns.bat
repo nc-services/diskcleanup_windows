27b9f6c8509c894d2fa153b90807561207ea5f3a
Eric Cheng <13010622+ericcheng05@users.noreply.github.com>
2022-10-26 11:25:32 +0800

    Create restart-service-dns.bat

---
+++ windows/restart-service-dns.bat
@@ -0,0 +1,9 @@
+cls
+@echo ===== START OF PROGRAM =====
+@echo.
+@echo ==== DO: DNS RESTART
+@net stop DNS
+@net start DNS
+@echo ==== END: DNS RESTART
+@echo.
+@echo ============= END OF PROGRAM =============
